0a76c4723ba739833acfdc22288e2b9ec862f319
sofiapaccapelo <sofipaccapelo@gmail.com>
2022-02-14 22:05:32 -0300

    estilos y funcion header

---
+++ script.js
@@ -0,0 +1,18 @@
+jQuery('document').ready(function($){
+
+    var menuBtn = $('.menu-icon'),
+        menu = $('.navegacion ul');
+
+    menuBtn.click(function() {
+
+        if( menu.hasClass('show') ) {
+
+            menu.removeClass('show');
+        } else {
+
+        menu.addClass('show');
+        }
+
+    });
+
+})
